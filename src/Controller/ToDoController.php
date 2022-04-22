9aee127a346e2d79faba37dff7fcf710cd903b70
yasmine <yasmine.gharbi@insat.ucar.tn>
2022-04-22 14:50:01 +0100

    Route_update

--- src/Controller/ToDoController.php
+++ src/Controller/ToDoController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 #[Route ('/todo')]
 class ToDoController extends AbstractController
 {
