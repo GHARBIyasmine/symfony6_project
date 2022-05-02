8dc7a84ccb12aea18da058a6ad4a54c5da381266
yasmine <yasmine.gharbi@insat.ucar.tn>
2022-05-02 22:45:22 +0100

    Doctrine 3 : remove + update entity

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -6,6 +6,7 @@ use App\Entity\Personne;
 use Doctrine\Persistence\ManagerRegistry;
 use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/personne')]
@@ -13,45 +14,45 @@ class PersonneController extends AbstractController
 {
 
     #[Route ('/', name: 'personne.list')]
-    public function index(ManagerRegistry $doctrine):Response {
-        $repository = $doctrine->getRepository(Personne::class);
-        $personnes = $repository->findAll();
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $manager = $doctrine->getRepository(Personne::class);
+        $personnes = $manager->findAll();
 
-     return $this->render('personne/index.html.twig', ['personnes'=>$personnes]);
+        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
     }
 
     #[Route ('/{page<\d+>?1}/{nbr<\d+>?2}', name: 'personne.list.by')]
-    public function find(ManagerRegistry $doctrine, $page , $nbr):Response {
-        $repository = $doctrine->getRepository(Personne::class);
-        $personnes = $repository->findby([],[] ,$nbr,(($page - 1)*$nbr));
+    public function find(ManagerRegistry $doctrine, $page, $nbr): Response
+    {
+        $manager = $doctrine->getRepository(Personne::class);
+        $personnes = $manager->findby([], [], $nbr, (($page - 1) * $nbr));
 
-        return $this->render('personne/index.html.twig', ['personnes'=>$personnes]);
+        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
     }
 
 
-
-
     #[Route ('/{id<\d+>}', name: 'personne.detail')]
-    public function detail(ManagerRegistry $doctrine, $id , Personne $personne = null):Response {
-//        $repository = $doctrine->getRepository(Personne::class);
-//        $personne = $repository->find($id);
+    public function detail(ManagerRegistry $doctrine, $id, Personne $personne = null): Response
+    {
+        $//        $manager = $doctrine->getRepository(Personne::class);
+        $//        $personne = $manager->find($id);
 
-        if (!$personne){
+        if (!$personne) {
 
             $this->addFlash('error', 'id not existent ');
             return $this->redirectToRoute('personne.list');
 
         }
 
-        return $this->render('personne/detail.html.twig', ['personne'=>$personne]);
+        return $this->render('personne/detail.html.twig', ['personne' => $personne]);
     }
-
-
 
 
     #[Route('/add', name: 'personne')]
     public function addPersonne(ManagerRegistry $doctrine): Response
-    {   $manager = $doctrine->getManager();
+    {
+        $manager = $doctrine->getManager();
         $personne = new Personne();
         $personne->setName('yasmine');
         $personne->setAge('20');
@@ -63,5 +64,42 @@ class PersonneController extends AbstractController
         return $this->render('personne/detail.html.twig', [
             'personne' => $personne,
         ]);
+    }
+
+
+    #[Route('/remove/{id<\d+>?0}', name: 'removePersonne')]
+    public function remove(ManagerRegistry $doctrine, Personne $personne = null): RedirectResponse
+    {
+        if (!$personne) {
+            $this->addFlash('error', 'id non existent');
+
+        } else {
+            $manager = $doctrine->getManager();
+            $manager->remove($personne);
+            $manager->flush();
+            $this->addFlash('success', 'entry removed successfully');
+        }
+        return $this->redirectToRoute('personne.list');
+    }
+
+
+    #[Route('/update/{id}/{name}/{lastname}/{age}', name: 'updatePersonne')]
+    public function update( $name, $lastname, $age,ManagerRegistry $doctrine,Personne $personne = null): RedirectResponse
+    {
+     if (!$personne){
+         $this->addFlash('error', 'id non existent');
+
+     }
+        else{
+            $personne->setName($name);
+            $personne->setLastname($lastname);
+            $personne->setAge($age);
+            $manager= $doctrine->getManager();
+            $manager->persist($personne);
+            $manager->flush();
+            $this->addFlash('success', 'entry updated successfully');
+        }
+
+     return $this->redirectToRoute('personne.list');
     }
 }
